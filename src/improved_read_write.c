2f7120d0ee7d2f31bcc81bf4cd5394d51ef8aa24
tyronevb <tvanballa@ska.ac.za>
2017-05-18 10:15:26 +0200

    Add support for bulk writes
    Implemented a new opcode that performs bulk writes. Maximum of 1988
    bytes can now be written per request / response transaction.

--- src/improved_read_write.c
+++ src/improved_read_write.c
@@ -10,13 +10,11 @@
 // function definitions
 
 //=================================================================================
-//	ImprovedRead
+//	BigReadWishboneCommandHandler
 //--------------------------------------------------------------------------------
 //	This method executes the BIG_READ_REG command. Allows for reading more than 4 bytes
 //  with a single transaction.
-//  
-//  The structures sNewOpCodeReqT and sNewOpCodeRespT should be defined in
-//  constant_defs.h
+// 
 //
 //	Parameter	Dir		Description
 //	---------	---		-----------
@@ -44,7 +42,7 @@ int BigReadWishboneCommandHandler(u8 * pCommand, u32 uCommandLength, u8 * uRespo
 		return XST_FAILURE;
 	}
 
-	// Create response packet
+	// Create response packet header
 	Response->Header.uCommandType = Command->Header.uCommandType + 1;
 	Response->Header.uSequenceNumber = Command->Header.uSequenceNumber;
 	Response->uStartAddressHigh = Command->uStartAddressHigh;
@@ -53,25 +51,26 @@ int BigReadWishboneCommandHandler(u8 * pCommand, u32 uCommandLength, u8 * uRespo
 
 	uAddress = (Command->uStartAddressHigh << 16) | (Command->uStartAddressLow);
 
-	xil_printf("Response packet created OK!\n\r");
+	//xil_printf("Response packet created OK!\n\r");
 
 	// Execute the command
 	for (uReadIndex = 0; uReadIndex < Command->uNumberOfReads; uReadIndex++)
 	{
 		// read 32-bit data
 		uReadData = Xil_In32(XPAR_AXI_SLAVE_WISHBONE_CLASSIC_MASTER_0_BASEADDR + uAddress);
+		// place data in response packet as it gets read
 		Response->uReadData[uDataIndex] = (uReadData >> 16) & 0xFFFF; // upper 16 bits
 		Response->uReadData[uDataIndex + 1] = uReadData & 0xFFFF; // lower 16 bits
 		uDataIndex = uDataIndex + 2; // increment data index
 		uAddress = uAddress + 4; // increment address by 4 to read the next 32-bit register
 	}
 
-	xil_printf("Big Read Completed OK!\n\r");
-	xil_printf("Data High: %d\n\r", Response->uReadData[0]);
-	xil_printf("Data Low: %d\n\r", Response->uReadData[1]);
+	//xil_printf("Big Read Completed OK!\n\r");
+	//xil_printf("Data High: %d\n\r", Response->uReadData[0]);
+	//xil_printf("Data Low: %d\n\r", Response->uReadData[1]);
 
-	// determine how many null words are required to fill up the packet - don't need this. can just use the last uData
-	xil_printf("Trying 994 words\n\r");
+	// determine how many null words are required to fill up the packet
+	//xil_printf("Trying 994 words\n\r");
 	uNullWords = 994 - (Command->uNumberOfReads)*2;
 
 	for (uNullWordsIndex = Command->uNumberOfReads*2; uNullWordsIndex < uNullWords; uNullWordsIndex++)
@@ -80,6 +79,84 @@ int BigReadWishboneCommandHandler(u8 * pCommand, u32 uCommandLength, u8 * uRespo
 	}
 
 	*uResponseLength = sizeof(sBigReadWishboneRespT);
+
+	return XST_SUCCESS;
+}
+
+//=================================================================================
+//	BigWriteWishboneCommandHandler
+//--------------------------------------------------------------------------------
+//	This method executes the BIG_WRITE_REG command. Allows for writing more than 4 bytes
+//  with a single transaction.
+//  
+//
+//	Parameter	Dir		Description
+//	---------	---		-----------
+//	pCommand				IN	Pointer to command header
+//	uCommandLength			IN	Length of command
+//	uResponsePacketPtr		IN	Pointer to where response packet must be constructed
+//	uResponseLength			OUT	Length of payload of response packet
+//
+//	Return
+//	------
+//	XST_SUCCESS if successful
+//=================================================================================
+int BigWriteWishboneCommandHandler(u8 * pCommand, u32 uCommandLength, u8 * uResponsePacketPtr, u32 * uResponseLength){
+
+	sBigWriteWishboneReqT *Command = (sBigWriteWishboneReqT *) pCommand;
+	sBigWriteWishboneRespT *Response = (sBigWriteWishboneRespT *) uResponsePacketPtr;
+	u32 uAddress;
+	u32 uWriteData;
+	u16 uWriteCount;
+	u16 uDataIndex = 0;
+	u16 uNumberOfWritesDone = 0;
+	u8 uPaddingIndex;
+
+	//xil_printf("Inside BigWriteWishboneCommandHandler now . . .\n\r");
+
+	if (uCommandLength < sizeof(sBigWriteWishboneReqT)){
+		return XST_FAILURE;
+	}
+
+	// determine the address at which to start 
+	uAddress = (Command->uStartAddressHigh << 16) | (Command->uStartAddressLow);
+
+	//xil_printf("Response packet created OK!\n\r");
+
+	// Execute the write command(s)
+	for (uWriteCount = 0; uWriteCount < Command->uNumberOfWrites; uWriteCount++)
+	{
+		// create 32-bit words from the 16-bit high, low words
+		// requires the 32-bit data to be sent in the form: [WORD1: 16-bit high, WORD1: 16-bit low, WORD2: 16-bit high, WORD2: 16-bit low . . .]
+		uWriteData = (Command->uWriteData[uDataIndex] << 16) | (Command->uWriteData[uDataIndex + 1]);
+		
+		// execute the write wishbone command
+		Xil_Out32(XPAR_AXI_SLAVE_WISHBONE_CLASSIC_MASTER_0_BASEADDR + uAddress, uWriteData);
+		
+		// debug msg
+		//xil_printf("Wrote data\n\r");
+		
+		uNumberOfWritesDone += 1;
+		uDataIndex = uDataIndex + 2; // increment data index to get the next 32-bit word
+		uAddress = uAddress + 4; // increment address by 4 to read the next 32-bit register
+	}
+
+	// debug msg
+	//xil_printf("Number of writes done: %d\n\r", uNumberOfWritesDone);
+	//xil_printf("Creating response packet. . .\n\r");
+
+	// Create response packet
+	Response->Header.uCommandType = Command->Header.uCommandType + 1;
+	Response->Header.uSequenceNumber = Command->Header.uSequenceNumber;
+	Response->uStartAddressHigh = Command->uStartAddressHigh;
+	Response->uStartAddressLow	= Command->uStartAddressLow;
+	Response->uNumberOfWritesDone = uNumberOfWritesDone;
+
+	// pad to minimum packet size and 64-bit boundary
+	for (uPaddingIndex = 0; uPaddingIndex < 6; uPaddingIndex++)
+		Response->uPadding[uPaddingIndex] = 0;
+
+	*uResponseLength = sizeof(sBigWriteWishboneRespT);
 
 	return XST_SUCCESS;
 }
