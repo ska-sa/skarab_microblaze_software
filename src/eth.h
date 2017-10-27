3c1d00731de25ced1d3e75f99cc6e2e56350b38b
rvw <rvanwyk@ska.ac.za>
2017-10-27 12:20:46 +0200

    Minor change - new Eth and IP headers
    Separated out the Ethernet and IP definitions into their own respective
    header files in order to promote better code reuse. This only affects
    pre-processing step.

---
+++ src/eth.h
@@ -0,0 +1,26 @@
+/**----------------------------------------------------------------------------
+*   FILE:       eth.h
+*   BRIEF:      Ethernet header definitions
+*
+*   DATE:       OCT 2017
+*
+*   COMPANY:    SKA SA
+*   AUTHOR:     R van Wyk
+*
+*   NOTES:      vim settings: "set sw=2 ts=2 expandtab autoindent"
+*------------------------------------------------------------------------------*/
+
+#ifndef _ETH_H_
+#define _ETH_H_
+
+#define ETH_FRAME_BASE              0
+#define ETH_DST_OFFSET              0
+#define ETH_DST_LEN                 6
+#define ETH_SRC_OFFSET              (ETH_DST_OFFSET + ETH_DST_LEN)  //6
+#define ETH_SRC_LEN                 6
+#define ETH_FRAME_TYPE_OFFSET       (ETH_SRC_OFFSET + ETH_SRC_LEN)  //12
+#define ETH_FRAME_TYPE_LEN          2
+
+#define ETH_FRAME_TOTAL_LEN         (ETH_FRAME_TYPE_OFFSET + ETH_FRAME_TYPE_LEN)  /* ethernet length = 14 */
+
+#endif
